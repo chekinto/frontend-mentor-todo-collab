c04cfbfd8912e6cdfdb397399419eaab81cf1c70
Ikeh Leonard <leoikeh99@gmail.com>
2020-12-13 23:33:19 +0100

    set up top and bottom background layouts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import Banner from './components/Banner'
-import Overview from './components/Overview'
-import CustomInput from './components/CustomInput'
+import React, { Fragment } from "react";
+import Overview from "./components/Overview";
+import CustomInput from "./components/CustomInput";
+import topImage from "./images/bg-desktop-light.jpg";
 
 const App = () => {
   return (
-    <>
-      <Banner />
+    <Fragment>
       <div className="container">
+        <div className="topImage">
+          <img src={topImage} alt="" />
+          <div className="bottomBg"></div>
+        </div>
+        {/* i think it will be better to have a todo component */}
         <Overview title="Todo" />
-        <CustomInput
-          type="text"
-          placeholder="Create a todo"
-
-        />
+        <CustomInput type="text" placeholder="Create a todo" />
       </div>
-    </>
-  )
-}
+    </Fragment>
+  );
+};
 
 export default App;
